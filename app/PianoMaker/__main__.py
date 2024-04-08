b0584561a04a5fe897138b04c636d7580f898c86
v-dvorak <vojtech.dvorak11@seznam.cz>
2024-04-08 23:03:41 +0200

    Clean up, add icon, organize code to main()

--- app/PianoMaker/__main__.py
+++ app/PianoMaker/__main__.py
@@ -3,48 +3,19 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from pathlib import Path
+import argparse
 
 from ..Parser.FileStructure import FileStructure
 from ..DatasetProcessor.DatasetProcessor import DatasetProcessor
 from ..Datasets.Import import AudioLabs_v2
 from ..DataMixer.DataMixer import DataMixer
-import argparse
-
-# ARGUMENT SETUP
-# TODO: description
-parser = argparse.ArgumentParser(
-    prog="Grand Staff Annotator",
-    # description="Compiles chosen OMR datasets into a big one for future use for training the YOLOv8 model.",
-    epilog=""
-    )
-
-# Required positional argument: output file name
-parser.add_argument("output", help="Path to store the final dataset at.")
-parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
-args = parser.parse_args()
-
-# DIRECTORIES INIT
-# set home for better navigation, everything is done in this working directory
-HOME = Path.absolute(Path(args.output) / "..").resolve()
-
-# create file structure to save data to
-processed_dir = HOME
-file_struct: FileStructure = FileStructure(HOME, "", "", "")
-
-LABELS = ["system_measures", "stave_measures", "staves"]
-
-# setup database, at this point in time only AudioLabs works
-data_proc = DatasetProcessor([AudioLabs_v2()], LABELS, file_struct)
-dat_mix: DataMixer = data_proc.load_dataset(AudioLabs_v2())
-names = [dat.name for dat in dat_mix.get_all_data()]
-names.sort()
-numbers = []
 
 class ImageViewer:
-    def __init__(self, master, image_paths):
+    def __init__(self, master: tk.Tk, image_paths: list[Path], verbose: bool = False):
         self.master = master
         self.image_paths = image_paths
         self.current_index = 0
+        self.verbose = verbose
 
         self.label = tk.Label(master)
         self.label.pack()
@@ -77,26 +48,30 @@ class ImageViewer:
 
     def process_and_next(self):
         data = self.input_var.get()
-        process_data(self.image_paths[self.current_index], data)
+        process_data(self.image_paths[self.current_index], data, self.verbose)
         self.current_index += 1
         self.show_next_image()
 
-def process_data(image_path, data):
+def process_data(image_path: Path, data: list[list[int]], verbose: bool = False):
     numbers.append(data)
-    print(f"image: {image_path}")
-    print("inputted data:", data)
-    print()
+    if verbose:
+        print(f"image: {image_path}")
+        print("inputted data:", data)
+        print()
 
 def main():
     # window setup
     image_paths = [x.img_path for x in dat_mix.get_all_data()]
     root = tk.Tk()
     root.title("Piano Maker")
+    root.wm_iconphoto(False, ImageTk.PhotoImage(Image.open("app/PianoMaker/icon.png")))
 
+    # window launch
     image_viewer = ImageViewer(root, image_paths)  # noqa: F841
 
     root.mainloop()
 
+    # end session
     with open(Path(args.output), "w", encoding="utf8") as file:
         for i in range(len(numbers)):
             file.write(names[i])
@@ -104,7 +79,33 @@ def main():
             file.write(numbers[i])
             file.write("\n")
 
-    print(f"Output saved to {Path(args.output).absolute()}")
+    print(f"Output saved to {Path(args.output).absolute().resolve()}")
 
 if __name__ == "__main__":
+    # ARGUMENT SETUP
+    # TODO: description
+    parser = argparse.ArgumentParser(
+        prog="Grand Staff Annotator",
+        # description="Compiles chosen OMR datasets into a big one for future use for training the YOLOv8 model.",
+        epilog=""
+        )
+
+    # Required positional argument: output file name
+    parser.add_argument("output", help="Path to store the final dataset at.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
+    args = parser.parse_args()
+
+    # DIRECTORIES INIT
+    # set home for better navigation, everything is done in this working directory
+    HOME = Path.absolute(Path(args.output) / "..").resolve()
+
+    file_struct: FileStructure = FileStructure(HOME, "", "", "")
+
+    # setup database, at this point in time only AudioLabs works
+    data_proc = DatasetProcessor([AudioLabs_v2()], [], file_struct)
+    dat_mix: DataMixer = data_proc.load_dataset(AudioLabs_v2())
+    names = [dat.name for dat in dat_mix.get_all_data()]
+    names.sort()
+    numbers = []
+
     main()
