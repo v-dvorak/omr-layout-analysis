9c5c0875ce00f6976b413e333d870d1cfbe45720
v-dvorak <vojtech.dvorak11@seznam.cz>
2024-04-09 18:42:57 +0200

    Implement Grand Staff label processor

---
+++ app/PianoMaker/PianoMaker.py
@@ -0,0 +1,126 @@
+
+#!/usr/bin/env python3
+from tqdm import tqdm
+from pathlib import Path
+from natsort import natsorted
+
+from ..Datasets.Import import Dataset_OMR
+from ..DataMixer.DataMixer import DataMixer
+from ..DataMixer.DatoInfo import DatoInfo
+from ..Parser.FileUtils import load_description_file_to_dictionary
+
+class PianoMaker:
+    """
+    Takes datasets to be processed, file structure and labels
+    and optional arguments that determine the final form of the dataset.
+    """
+    # data control
+    _LABELS: list[str] = ["system_measures", "stave_measures", "staves", "system", "grand_staff"]
+
+    def __init__(self,
+                 dataset: Dataset_OMR,
+                 dataset_path: Path,
+                 output_path: Path,
+                 piano_path: Path,
+                 offset: int = 10,
+                 grand_limit: int = 0,
+                 verbose: bool = False,) -> None:
+        
+        self._DATASET: Dataset_OMR = dataset
+        self._PIANO_ANNOT: list[list[int]] = None
+
+        self.dataset_path: Path = dataset_path
+        self.output_path: Path = output_path
+        self.output_path_img: Path = None
+        self.output_path_json: Path = None
+        self.piano_path: Path = piano_path
+
+        self._offset: int = offset
+        self._grand_limit: int = grand_limit
+        self._verbose: bool = verbose
+
+    def print_datataset_names(self):
+        """
+        Prints out names of given datasets.
+        """
+        print("Following datasets will be processed:")
+        print(self._DATASET.name)
+
+    def _create_file_structure(self):
+        # Create the base directory if it doesn't exist
+        self.output_path.mkdir(parents=True, exist_ok=True)
+
+        # Create two subdirectories inside the base directory
+        self.output_path_img = self.output_path / "img"
+        self.output_path_json = self.output_path / "json"
+
+        self.output_path_img.mkdir(exist_ok=True)
+        self.output_path_json.mkdir(exist_ok=True)
+    
+    def process_dataset(self):
+        self._create_file_structure()
+        dat = self.load_dataset()
+        self._PIANO_ANNOT = load_description_file_to_dictionary(self.piano_path)
+        self._process_part_of_dataset(self._DATASET, dat.get_all_data(), self.output_path_img, self.output_path_json)
+        
+    def load_dataset(self) -> DataMixer:
+        """
+        Loads given dataset to a list of `DatoInfo`s.
+
+        Args:
+        - dataset to be loaded
+
+        Returns:
+        - `DataMixer` loaded with records from given dataset
+        """
+        dat = DataMixer()
+        all_images_paths: list[Path] = natsorted(((self.dataset_path).rglob("*.png")), key=str)
+        all_labels_paths: list[Path] = natsorted(((self.dataset_path).rglob("*.json")), key=str)
+        dat.process_file_dump(all_images_paths, all_labels_paths)
+        return dat
+
+    def _process_part_of_dataset(self,
+                                 current_dataset: Dataset_OMR,
+                                 data: list[DatoInfo],
+                                 image_path: Path,
+                                 label_path: Path,
+                                 tag: str = ""):
+        """
+        Base method for all other dataset file processing.
+        Processes all files given in a `DataMixer` format.
+
+        All used directories have to be setup BEFORE this method is called!
+
+        Args:
+        - dataset to be processed
+        - `DataMixer` loaded with records from given dataset
+        - path to save images to
+        - path to save labels to
+        - optional:
+            - tags generated files at the beginning of their name with dataset nickname, e.g.: `\"al2_filename\"
+        """
+
+        for dato in tqdm(data):
+            if self._verbose:
+                print(dato.name)
+                print(dato.img_path.parts[-1], dato.label_path.parts[-1])
+            
+            current_dataset.process_image(
+                        dato.img_path,
+                        image_path / (tag + dato.name + ".png")
+                    )
+            current_dataset.preprocess_label(
+                        dato.label_path,
+                        label_path / (tag + dato.name + ".json"),
+                        self._LABELS,
+                        piano=self._PIANO_ANNOT[dato.name],
+                        deduplicate=True,
+                        grand_limit=self._grand_limit,
+                        offset=self._offset
+                    )
+    
+    def print_final_message(self):
+        print(f"Job finished, results are at {self.output_path.absolute().resolve()}")
+        print(f"Processed dataset: {self._DATASET.name}")
+        print(f"With offset: {self._offset}")
+        print(f"Minimal staves for grand staff: {self._grand_limit}")
--- app/PianoMaker/__main__.py
+++ app/PianoMaker/__main__.py
@@ -1,111 +1,58 @@
 #!/usr/bin/env python3
 
-import tkinter as tk
-from PIL import Image, ImageTk
-from pathlib import Path
 import argparse
+from pathlib import Path
 
-from ..Parser.FileStructure import FileStructure
-from ..DatasetProcessor.DatasetProcessor import DatasetProcessor
-from ..Datasets.Import import AudioLabs_v2
-from ..DataMixer.DataMixer import DataMixer
+from ..Datasets.Import import Dataset_OMR
+from .PianoMaker import PianoMaker
 
-class ImageViewer:
-    def __init__(self, master: tk.Tk, image_paths: list[Path], verbose: bool = False):
-        self.master = master
-        self.image_paths = image_paths
-        self.current_index = 0
-        self.verbose = verbose
+# ARGUMENT SETUP
+# TODO: description
+parser = argparse.ArgumentParser(
+    prog="OMR Dataset Parser",
+    description="Compiles chosen OMR datasets into a big one for future use for training the YOLOv8 model.",
+    epilog=""
+    )
 
-        self.label = tk.Label(master)
-        self.label.pack()
+# Required positional argument: output file name
+parser.add_argument("dataset_path", help="Path to dataset to be processed.")
+parser.add_argument("piano_path", help="Path to grand staff annotations.")
+parser.add_argument("output_path", help="Path to store the final dataset at.")
 
-        self.input_var = tk.StringVar()
-        self.entry = tk.Entry(master, textvariable=self.input_var)
-        self.entry.pack()
+parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose.")
+parser.add_argument("-o", "--offset", default=10, help="How far apart two coordinates can be to still be considered the same, in pixels. Default is 10.")
+parser.add_argument("-l", "--grand_limit", default=1, help="Minimal amount of staves to consider multiple staves linked together to be a grand staff. Default is 1.")
 
-        self.submit_button = tk.Button(master, text="Submit", command=self.process_and_next)
-        self.submit_button.pack()
 
-        # bind the Enter key to the process_and_next method - enter skips to next image
-        self.master.bind("<Return>", lambda event: self.process_and_next())
+# DATASETS INIT
+dataset_database = Dataset_OMR.__subclasses__() # Python magic
+for i in range(len(dataset_database)):
+    dataset_database[i] = dataset_database[i]()
 
-        self.show_next_image()
+# ADD OPTIONS TO ARGPARSE
+# add arguments for datasets
+for current_dataset in dataset_database:
+    parser.add_argument("--" + current_dataset.nickname,
+                        action="store_true",
+                        help=f"The {current_dataset.name} dataset will be processed.")
 
-    def show_next_image(self):
-        if self.current_index < len(self.image_paths):
-            image_path = self.image_paths[self.current_index]
+args = parser.parse_args()
 
-            # load and display image
-            image = Image.open(image_path)
-            image.thumbnail((self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
-            photo = ImageTk.PhotoImage(image)
-            self.label.config(image=photo)
-            self.label.image = photo
-        else:
-            # close window, all images were processed
-            self.master.destroy()
+# get dataset that will be processed
+datasets_to_work_with: list[Dataset_OMR] = []
+for current_dataset in dataset_database:
+    if getattr(args, current_dataset.nickname):
+        datasets_to_work_with.append(current_dataset)
 
-    def process_and_next(self):
-        data = self.input_var.get()
-        process_data(self.image_paths[self.current_index], data, self.verbose)
-        self.current_index += 1
-        self.show_next_image()
+if datasets_to_work_with == []:
+    print("No datasets were specified, quitting job.")
+    quit()
+if len(datasets_to_work_with) > 1:
+    print("Too many datasets were specified, specify only one at a time, quitting job.")
+    quit()
 
-def process_data(image_path: Path, data: list[list[int]], verbose: bool = False):
-    numbers.append(data)
-    if verbose:
-        print(f"image: {image_path}")
-        print("inputted data:", data)
-        print()
+pm = PianoMaker(datasets_to_work_with[0], Path(args.dataset_path), Path(args.output_path), Path(args.piano_path),
+                offset=int(args.offset), grand_limit=int(args.grand_limit), verbose=args.verbose)
 
-def main():
-    # window setup
-    image_paths = [x.img_path for x in dat_mix.get_all_data()]
-    root = tk.Tk()
-    root.title("Piano Maker")
-    root.wm_iconphoto(False, ImageTk.PhotoImage(Image.open("app/PianoMaker/icon.png")))
-
-    # window launch
-    image_viewer = ImageViewer(root, image_paths)  # noqa: F841
-
-    root.mainloop()
-
-    # end session
-    with open(Path(args.output), "w", encoding="utf8") as file:
-        for i in range(len(numbers)):
-            file.write(names[i])
-            file.write("\n")
-            file.write(numbers[i])
-            file.write("\n")
-
-    print(f"Output saved to {Path(args.output).absolute().resolve()}")
-
-if __name__ == "__main__":
-    # ARGUMENT SETUP
-    # TODO: description
-    parser = argparse.ArgumentParser(
-        prog="Grand Staff Annotator",
-        # description="Compiles chosen OMR datasets into a big one for future use for training the YOLOv8 model.",
-        epilog=""
-        )
-
-    # Required positional argument: output file name
-    parser.add_argument("output", help="Path to store the final dataset at.")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Make script verbose")
-    args = parser.parse_args()
-
-    # DIRECTORIES INIT
-    # set home for better navigation, everything is done in this working directory
-    HOME = Path.absolute(Path(args.output) / "..").resolve()
-
-    file_struct: FileStructure = FileStructure(HOME, "", "", "")
-
-    # setup database, at this point in time only AudioLabs works
-    data_proc = DatasetProcessor([AudioLabs_v2()], [], file_struct)
-    dat_mix: DataMixer = data_proc.load_dataset(AudioLabs_v2())
-    names = [dat.name for dat in dat_mix.get_all_data()]
-    names.sort()
-    numbers = []
-
-    main()
+pm.process_dataset()
+pm.print_final_message()
