dac9ba0abbcb0e35d22b0a32f6cc2ffb6299059f
v-dvorak <vojtech.dvorak11@seznam.cz>
2024-05-08 17:05:10 +0200

    Finish build and download Python scripts

--- app/Utils/build.py
+++ app/Utils/build.py
@@ -1,12 +1,17 @@
 import os
+from datetime import date
+from pathlib import Path
 
 from .BColors import print_done, print_header
 from .download import download
 
 
 def build():
+    # download datasets
     download()
-    print_header("Preprocessing datasets, finding systems and grand staffs:")
+
+    # convert AL2 and MPP to COCO format, add systems and grand staffs
+    print_header("Preprocessing datasets, analyzing systems and grand staffs:")
     piano_dats = [
         ("Muscima++", "MPP_grand_staff.txt", "mpp_gs", "--mpp"),
         ("AudioLabs_v2", "AL2_grand_staff.txt", "al2_gs", "--al2"),
@@ -15,6 +20,14 @@ def build():
         os.system(f"python3 -m app.PianoMaker ./datasets/{name} {annot_path} ./datasets/{out_path} {specs}")
         print_done(f"Preprocessing {name} done.")
 
+    # using MuseScore create PNGs adn SVGs for OSLiC, convert to COCO format
     print_header("Preprocessing OSLiC dataset:")
     os.system(f"python3 -m app.Synthetic --png --svg")
     print_done("Preprocessing OSLiC done.")
+
+    # collect all datasets and combine them into a single one
+    print_header("Combining all datasets into one.")
+    dataset_name = f"{date.today().strftime('%Y_%m_%d')}_final_dataset"
+    os.system(
+        f"python3 -m app ./datasets/{dataset_name} --stad ./datasets/al2_gs ./datasets/mpp_gs ./datasets/Lieder-main")
+    print_done(f"Datasets combined successfully, the final dataset is at {Path(dataset_name).resolve().absolut()}")
--- app/Utils/download.py
+++ app/Utils/download.py
@@ -5,5 +5,5 @@ from .BColors import print_done, print_header
 
 def download():
     print_header("Downloading datasets:")
-    os.system("python3 -m app.DatasetClasses --osl")
+    os.system("python3 -m app.DatasetClasses --al2 --mpp --osl")
     print_done("Datasets downloaded.")
